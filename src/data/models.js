f89b68e7fa9d3563b63b7c394d3bc6a113ed95a2
Michal Lacki <mlacki@gmail.com>
2016-12-14 16:01:43 +0100

    brands created

--- src/data/models.js
+++ src/data/models.js
@@ -1,3 +1,21 @@
 /**
  * Created by mlacki on 14.12.16.
  */
+
+export default [
+  {
+    id: 1,
+    name: 'Opel',
+    modelsIds: [1, 2, 3]
+  },
+  {
+    id: 2,
+    name: 'Ford',
+    modelsIds: [4, 5, 6]
+  },
+  {
+    id: 3,
+    name: 'Fiat',
+    modelsIds: [7, 8, 9]
+  }
+]
