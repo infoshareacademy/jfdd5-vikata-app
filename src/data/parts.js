e9f6d9fd3abd9e7b63093423250b09eb4a1c6200
Alwox <alanwolejko@gmail.com>
2016-12-14 16:39:20 +0100

    poprawa listy hurtowni

--- src/data/parts.js
+++ src/data/parts.js
@@ -6,80 +6,60 @@ export default [
     id: 1,
     name: 'Michelin CrossClimate',
     type: tyre,
-    shop2: [
-      {
-        shopId: 1,
-        price: 10
-      },
-      {
-        shopId: 2,
-        price: 40
-      }
-    ],
-    shop: [1,2],
     description: ['Całoroczna, szerokość 205, rozmiar 16']
   },
   {
     id: 2,
     name: 'Zarówka LED',
     type: bulb,
-    shop: [1],
     description: ['Wymiary: 156 x 18 x 42mm,Barwa światła: 5000K']
   },
   {
     id: 3,
     name: 'Reflektor główny lewy',
     type: bulb,
-    shop: [2,3],
     description: ['Przedni lewy główny reflektor z żarówką HB1 9004']
   },
   {
     id: 4,
     name: 'Goodyear Wrangler Duratrac',
     type: tyre,
-    shop: [1,3],
     description: [Całoroczna, szerokość 235, rozmiar 15]
   },
   {
     id: 5,
     name: 'Dębica Navigator',
     type: tyre,
-    shop: [1,3],
     description: [Zimowa, szerokość 195, rozmiar 16]
   },
   {
     id: 6,
     name: 'Radio ',
     type: audio,
-    shop: [4,7],
     description: [Całoroczna, szerokość 205, rozmiar 16]
   },
   {
     id: 7,
     name: 'Michelin CrossClimate',
     type: tyre,
-    shop: [4,7],
     description: [Całoroczna, szerokość 205, rozmiar 16]
   },
   {
     id: 8,
     name: 'Michelin CrossClimate',
     type: tyre,
-    shop: [4,7],
     description: [Całoroczna, szerokość 205, rozmiar 16]
   },
   {
     id: 9,
     name: 'Michelin CrossClimate',
     type: tyre,
-    shop: [4,7],
     description: [Całoroczna, szerokość 205, rozmiar 16]
   },
   {
     id: 10,
     name: 'Michelin CrossClimate',
     type: tyre,
-    shop: [4,7],
     description: [Całoroczna, szerokość 205, rozmiar 16]
   },
 ]
