8ac95ce410ca4ce89bf072f3f8f617a9e672a5d8
Rafal Miler <rafalmiler77@gmail.com>
2016-12-14 16:38:10 +0100

    parts completed

--- src/data/parts.js
+++ src/data/parts.js
@@ -6,80 +6,70 @@ export default [
     id: 1,
     name: 'Michelin CrossClimate',
     type: tyre,
-    shop2: [
-      {
-        shopId: 1,
-        price: 10
-      },
-      {
-        shopId: 2,
-        price: 40
-      }
-    ],
-    shop: [1,2],
     description: ['Całoroczna, szerokość 205, rozmiar 16']
   },
   {
     id: 2,
     name: 'Zarówka LED',
     type: bulb,
-    shop: [1],
     description: ['Wymiary: 156 x 18 x 42mm,Barwa światła: 5000K']
   },
   {
     id: 3,
     name: 'Reflektor główny lewy',
     type: bulb,
-    shop: [2,3],
     description: ['Przedni lewy główny reflektor z żarówką HB1 9004']
   },
   {
     id: 4,
     name: 'Goodyear Wrangler Duratrac',
     type: tyre,
-    shop: [1,3],
-    description: [Całoroczna, szerokość 235, rozmiar 15]
+    description: ['Całoroczna, szerokość 235, rozmiar 15']
   },
   {
     id: 5,
     name: 'Dębica Navigator',
     type: tyre,
-    shop: [1,3],
-    description: [Zimowa, szerokość 195, rozmiar 16]
+    description: ['Zimowa, szerokość 195, rozmiar 16']
   },
   {
     id: 6,
-    name: 'Radio ',
+    name: 'Radio Pioneer MVH-180UB',
     type: audio,
-    shop: [4,7],
-    description: [Całoroczna, szerokość 205, rozmiar 16]
+    description: ['Uzyskaj 4 x 50W czystej mocy i doskonałą reprodukcję dźwięku.' +
+    ' Gniazdo USB pozwala na podłączenie przenośnej pamięci,' +
+    ' a port AUX-in umożliwia podłączenie dowolnego urządzenia audio.' +
+    ' MVH-180UB oferuje wsparcie dla audiofilskiego formatu bezstratnych plików FLAC,' +
+    ' które zapewniają słuchanie muzyki w najwyższej jakości']
   },
   {
     id: 7,
-    name: 'Michelin CrossClimate',
-    type: tyre,
-    shop: [4,7],
-    description: [Całoroczna, szerokość 205, rozmiar 16]
+    name: 'Sony MEX-N5100BT',
+    type: audio,
+    description: ['Radioodtwarzacz MEX-N5100BT odczyta na głos odebrane maile' +
+    ' i pozwoli głosowo zredagować odpowiedzi. Nie odrywasz więc wzroku od drogi,' +
+    ' a i tak wiesz, co się dzieje. Nowy, dwustrefowy system Dynamic Colour Illuminator' +
+    ' pozwala na wyświetlenie maksymalnie 35 000 kolorów zmieniających się w rytm muzyki.']
   },
   {
     id: 8,
-    name: 'Michelin CrossClimate',
-    type: tyre,
-    shop: [4,7],
-    description: [Całoroczna, szerokość 205, rozmiar 16]
+    name: 'Sprzęgło jednokierunkowe alternatora Dodge Nitro CRD',
+    type: engine,
+    description: ['Jednokierunkowe sprzęgiełko alternatora służy w silniku spalinowym' +
+    ' do odłączania alternatora od wału korbowego, który cechuje się nieregularnym ruchem obrotowym,' +
+    ' ponieważ alternator ma najwyższy moment bezwładności w pomocniczym układzie napędu pasowego.' +
+    ' Dlatego też alternator jest napędzany wyłącznie składową przyspieszenia ruchu obrotowego wału korbowego.']
   },
   {
     id: 9,
-    name: 'Michelin CrossClimate',
-    type: tyre,
-    shop: [4,7],
-    description: [Całoroczna, szerokość 205, rozmiar 16]
+    name: 'Hertz DCX 165.3',
+    type: audio,
+    description: ['głośniki 2-drożne 120W']
   },
   {
     id: 10,
-    name: 'Michelin CrossClimate',
-    type: tyre,
-    shop: [4,7],
-    description: [Całoroczna, szerokość 205, rozmiar 16]
+    name: 'Pasek wielorowkowy 5070680',
+    type: engine,
+    description: ['Pasek wielorowkowy 5070680 polecany przez Hołowczyca']
   },
 ]
