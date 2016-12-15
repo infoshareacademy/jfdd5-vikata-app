cb981c366e34b54c06c97c16c1119f26b00aba1b
Rafal Miler <rafalmiler77@gmail.com>
2016-12-15 13:09:14 +0100

    types removed

--- src/data/parts.js
+++ src/data/parts.js
@@ -5,37 +5,31 @@ export default [
   {
     id: 1,
     name: 'Michelin CrossClimate',
-    type: tyre,
     description: ['Całoroczna, szerokość 205, rozmiar 16']
   },
   {
     id: 2,
     name: 'Zarówka LED',
-    type: bulb,
     description: ['Wymiary: 156 x 18 x 42mm,Barwa światła: 5000K']
   },
   {
     id: 3,
     name: 'Reflektor główny lewy',
-    type: bulb,
     description: ['Przedni lewy główny reflektor z żarówką HB1 9004']
   },
   {
     id: 4,
     name: 'Goodyear Wrangler Duratrac',
-    type: tyre,
     description: ['Całoroczna, szerokość 235, rozmiar 15']
   },
   {
     id: 5,
     name: 'Dębica Navigator',
-    type: tyre,
     description: ['Zimowa, szerokość 195, rozmiar 16']
   },
   {
     id: 6,
     name: 'Radio Pioneer MVH-180UB',
-    type: audio,
     description: ['Uzyskaj 4 x 50W czystej mocy i doskonałą reprodukcję dźwięku.' +
     ' Gniazdo USB pozwala na podłączenie przenośnej pamięci,' +
     ' a port AUX-in umożliwia podłączenie dowolnego urządzenia audio.' +
@@ -45,7 +39,6 @@ export default [
   {
     id: 7,
     name: 'Sony MEX-N5100BT',
-    type: audio,
     description: ['Radioodtwarzacz MEX-N5100BT odczyta na głos odebrane maile' +
     ' i pozwoli głosowo zredagować odpowiedzi. Nie odrywasz więc wzroku od drogi,' +
     ' a i tak wiesz, co się dzieje. Nowy, dwustrefowy system Dynamic Colour Illuminator' +
@@ -54,7 +47,6 @@ export default [
   {
     id: 8,
     name: 'Sprzęgło jednokierunkowe alternatora Dodge Nitro CRD',
-    type: engine,
     description: ['Jednokierunkowe sprzęgiełko alternatora służy w silniku spalinowym' +
     ' do odłączania alternatora od wału korbowego, który cechuje się nieregularnym ruchem obrotowym,' +
     ' ponieważ alternator ma najwyższy moment bezwładności w pomocniczym układzie napędu pasowego.' +
@@ -63,13 +55,11 @@ export default [
   {
     id: 9,
     name: 'Hertz DCX 165.3',
-    type: audio,
     description: ['głośniki 2-drożne 120W']
   },
   {
     id: 10,
     name: 'Pasek wielorowkowy 5070680',
-    type: engine,
     description: ['Pasek wielorowkowy 5070680 polecany przez Hołowczyca']
   },
 ]
