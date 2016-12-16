70631049005ad028155bd2d06cd8d25e2e630cab
Michal Lacki <mlacki@gmail.com>
2016-12-16 16:17:22 +0100

    Brands and models search work with this.state

--- src/CarSelectionForm/CarSelectionForm.js
+++ src/CarSelectionForm/CarSelectionForm.js
@@ -1,85 +1,74 @@
 /**
  * Created by mlacki on 14.12.16.
  */
-import {SplitButton, MenuItem} from 'react-bootstrap';
-import React from 'react'
+import {SplitButton, MenuItem} from "react-bootstrap";
+import React from "react";
+import {brands, models} from "../data";
 
 // import { Link } from 'react-router'
 
 //
-import {brands} from '../data'
-import {models} from '../data'
 const BUTTONS = ['Default', 'Primary', 'Success', 'Info', 'Warning', 'Danger'];
 
 export default class CarSelectionForm extends React.Component {
 
   constructor() {
-    super ()
-
+    super()
 
     this.handleBrandClick = eventKey => {
-      console.log(eventKey)
+      var selectedBrandData = brands.find(brand => brand.id === eventKey);
       this.setState({
-        brand: brands.find( brand => brand.id === eventKey ).name
+        brand: selectedBrandData
       })
     }
 
     this.state = {
-      brand: {name:'MARKA'},
-      model: 'MODEL'
+      brand: {name: 'wybierz markę'},
+      model: {name: 'wybierz model'}
     }
 
-    this.modelBrandClick = eventKey => {
-      console.log(eventKey)
+
+    this.handleModelClick = eventKey => {
+      var selectedModel = models.find(model => model.id === eventKey);
       this.setState({
-        model: models.find( model => model.id === eventKey).name
+        model: selectedModel
 
       })
     }
-
   }
 
-  render () {
+  render() {
 
     const brandsListItems = brands.map(
-      brand  =>
-       <MenuItem eventKey={brand.id}>{brand.name} </MenuItem>
-    )
+      brand =>
+        <MenuItem eventKey={brand.id}>{brand.name} </MenuItem>
+    );
 
-
-      const modelsListItems = models.filter(
-        model => this.state.brand.modelsIds ? this.state.brand.modelsIds.indexOf(model.id) !== -1 : false
-      )
-
-    // )
-    //   modelId => models.find(
-    //     model => model.id === modelId)
-    //   ).map(models =>
-    //     <MenuItem key={models.id}> {models.name} </MenuItem>
-    //
-    //   )
-
-
-
+    const modelsListItems = models.filter(
+      model => this.state.brand.modelsIds ? this.state.brand.modelsIds.indexOf(model.id) !== -1 : false
+    ).map(
+      model =>
+        <MenuItem eventKey={model.id}>{model.name} </MenuItem>
+    );
 
     return (
 
-    <div>
-      <h1> SEARCH BY MODEL </h1>
+      <div>
+        <h1> SEARCH BY MODEL </h1>
 
-      <SplitButton bsStyle="1" title={this.state.brand.name} key={1} id={`split-button-basic-1`} onSelect={this.handleBrandClick}>
-        {brandsListItems}
-      </SplitButton>
+        <SplitButton bsStyle="1" title={this.state.brand.name} key={1} id={`split-button-basic-1`}
+                     onSelect={this.handleBrandClick}>
+          {brandsListItems}
+        </SplitButton>
 
-      <SplitButton bsStyle="1" title={this.state.model} key={2} id={`split-button-basic-1`}>
-        {modelsListItems}
-      </SplitButton>
+        <SplitButton bsStyle="1" title={this.state.model.name} key={2} id={`split-button-basic-1`}
+                     onSelect={this.handleModelClick}>
+          {modelsListItems}
+        </SplitButton>
 
-
-    </div>
+      </div>
 
     )
   }
 
 }
-
