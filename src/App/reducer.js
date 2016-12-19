810a2e8dfb632ce569d962b931042cde61952e93
Michal Lacki <mlacki@gmail.com>
2016-12-19 15:36:29 +0100

    reducer in APP folder created

---
+++ src/App/reducer.js
@@ -0,0 +1,27 @@
+
+import {brands, models, parts, shops, types} from "../data"
+
+
+const initialState = {
+  brands: brands,
+  models: models,
+  parts: parts,
+  shops: shops,
+  types: types
+
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case 'SET_FILTER':
+      return {
+       ...state,
+        filterNames: state.filterNames.concat(action.filterName)
+      }
+
+
+
+    default:
+      return state
+  }
+}
