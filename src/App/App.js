7d367ef948c31f25bde22af6711973e37ad6c561
Rafał Miler <rafalmiler77@gmail.com>
2017-01-11 23:24:16 +0100

    header banner removed temporarily

--- src/App/App.js
+++ src/App/App.js
@@ -14,16 +14,6 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <PageHeader bsClass="pageHeader">Example page header <small>Subtext for header</small></PageHeader>
-
-        {/*<Image src={process.env.PUBLIC_URL + '/img/img-header/headerBaner.jpg'}*/}
-             {/*role="presentation"*/}
-        {/*/>*/}
-        <div className="wide">
-          <div className="col-xs-5 line"><hr/></div>
-          <div className="col-xs-2 logo">Logo</div>
-          <div className="col-xs-5 line"><hr/></div>
-        </div>
       <CarSelectionForm/>
         <TypesView/>
           {this.props.children}
