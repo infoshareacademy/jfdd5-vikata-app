c1e2ecb487eb531a12cc109dabdaf364f5831e56
Alwox <alanwolejko@gmail.com>
2017-01-11 16:31:31 +0100

    podział danych części na 2 listy i przebudowanie ich

---
+++ src/data/partsToSell.js
@@ -0,0 +1,69 @@
+/**
+ * Created by rafalmiler on 14.12.16.
+ */
+export default [
+  {
+    id: 1,
+    typeId: 1,
+    name: 'Działo Jonowe "Miotacz 2000"',
+    description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam blandit cursus augue sed eleifend. Donec mattis massa sit amet porttitor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer vehicula tristique laoreet. Quisque nisi eros, scelerisque sed nibh eu, pellentesque aliquet quam. Nullam euismod mattis mi, nec ultricies turpis imperdiet ac. Sed molestie massa erat, ut ullamcorper arcu scelerisque eu.'],
+    price: 120,
+    image: 'part01.jpg'
+  },
+  {
+    id: 2,
+    typeId: 2,
+    name: 'Klakson-miś',
+    description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam blandit cursus augue sed eleifend. Donec mattis massa sit amet porttitor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer vehicula tristique laoreet. Quisque nisi eros, scelerisque sed nibh eu, pellentesque aliquet quam. Nullam euismod mattis mi, nec ultricies turpis imperdiet ac. Sed molestie massa erat, ut ullamcorper arcu scelerisque eu.'],
+    price: 120,
+    image: 'part02.jpg'
+  },
+  {
+    id: 3,
+    typeId: 2,
+    name: 'Fotel z toaletą',
+    description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam blandit cursus augue sed eleifend. Donec mattis massa sit amet porttitor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer vehicula tristique laoreet. Quisque nisi eros, scelerisque sed nibh eu, pellentesque aliquet quam. Nullam euismod mattis mi, nec ultricies turpis imperdiet ac. Sed molestie massa erat, ut ullamcorper arcu scelerisque eu.'],
+    price: 120,
+    image: 'part03.jpg'
+  },
+  {
+    id: 4,
+    typeId: 1,
+    name: 'Napęd rakietowy "Turbo wystrzał"',
+    description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam blandit cursus augue sed eleifend. Donec mattis massa sit amet porttitor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer vehicula tristique laoreet. Quisque nisi eros, scelerisque sed nibh eu, pellentesque aliquet quam. Nullam euismod mattis mi, nec ultricies turpis imperdiet ac. Sed molestie massa erat, ut ullamcorper arcu scelerisque eu.'],
+    price: 120,
+    image: 'part04.jpg'
+  },
+  {
+    id: 5,
+    typeId: 1,
+    name: 'Dębica Navigator',
+    description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam blandit cursus augue sed eleifend. Donec mattis massa sit amet porttitor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer vehicula tristique laoreet. Quisque nisi eros, scelerisque sed nibh eu, pellentesque aliquet quam. Nullam euismod mattis mi, nec ultricies turpis imperdiet ac. Sed molestie massa erat, ut ullamcorper arcu scelerisque eu.'],
+    price: 120,
+    image: 'part05.jpg'
+  },
+  {
+    id: 6,
+    typeId: 3,
+    name: 'Radio Pioneer MVH-180UB',
+    description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam blandit cursus augue sed eleifend. Donec mattis massa sit amet porttitor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer vehicula tristique laoreet. Quisque nisi eros, scelerisque sed nibh eu, pellentesque aliquet quam. Nullam euismod mattis mi, nec ultricies turpis imperdiet ac. Sed molestie massa erat, ut ullamcorper arcu scelerisque eu.'],
+    price: 120,
+    image: 'part06.jpg'
+  },
+  {
+    id: 7,
+    typeId: 3,
+    name: 'Sony MEX-N5100BT',
+    description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam blandit cursus augue sed eleifend. Donec mattis massa sit amet porttitor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer vehicula tristique laoreet. Quisque nisi eros, scelerisque sed nibh eu, pellentesque aliquet quam. Nullam euismod mattis mi, nec ultricies turpis imperdiet ac. Sed molestie massa erat, ut ullamcorper arcu scelerisque eu.'],
+    price: 120,
+    image: 'part07.jpg'
+  },
+  {
+    id: 8,
+    typeId: 4,
+    name: 'Sprzęgło jednokierunkowe alternatora Dodge Nitro CRD',
+    description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam blandit cursus augue sed eleifend. Donec mattis massa sit amet porttitor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer vehicula tristique laoreet. Quisque nisi eros, scelerisque sed nibh eu, pellentesque aliquet quam. Nullam euismod mattis mi, nec ultricies turpis imperdiet ac. Sed molestie massa erat, ut ullamcorper arcu scelerisque eu.'],
+    price: 120,
+    image: 'part08.jpg'
+  }
+]
---
+++ src/data/partsWanted.js
@@ -0,0 +1,61 @@
+/**
+ * Created by alanw on 11.01.2017.
+ */
+export default [
+  {
+    id: 1,
+    typeId: 1,
+    name: 'Działo Jonowe "Miotacz 2000"',
+    description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam blandit cursus augue sed eleifend. Donec mattis massa sit amet porttitor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer vehicula tristique laoreet. Quisque nisi eros, scelerisque sed nibh eu, pellentesque aliquet quam. Nullam euismod mattis mi, nec ultricies turpis imperdiet ac. Sed molestie massa erat, ut ullamcorper arcu scelerisque eu.'],
+    price: 120,
+    image: 'partW01.jpg'
+  },
+  {
+    id: 2,
+    typeId: 2,
+    name: 'Klakson-miś',
+    description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam blandit cursus augue sed eleifend. Donec mattis massa sit amet porttitor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer vehicula tristique laoreet. Quisque nisi eros, scelerisque sed nibh eu, pellentesque aliquet quam. Nullam euismod mattis mi, nec ultricies turpis imperdiet ac. Sed molestie massa erat, ut ullamcorper arcu scelerisque eu.'],
+    price: 120,
+    image: 'partW02.jpg'
+  },
+  {
+    id: 3,
+    typeId: 2,
+    name: 'Fotel z toaletą',
+    description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam blandit cursus augue sed eleifend. Donec mattis massa sit amet porttitor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer vehicula tristique laoreet. Quisque nisi eros, scelerisque sed nibh eu, pellentesque aliquet quam. Nullam euismod mattis mi, nec ultricies turpis imperdiet ac. Sed molestie massa erat, ut ullamcorper arcu scelerisque eu.'],
+    price: 120,
+    image: 'partW03.jpg'
+  },
+  {
+    id: 4,
+    typeId: 1,
+    name: 'Napęd rakietowy "Turbo wystrzał"',
+    description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam blandit cursus augue sed eleifend. Donec mattis massa sit amet porttitor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer vehicula tristique laoreet. Quisque nisi eros, scelerisque sed nibh eu, pellentesque aliquet quam. Nullam euismod mattis mi, nec ultricies turpis imperdiet ac. Sed molestie massa erat, ut ullamcorper arcu scelerisque eu.'],
+    price: 120,
+    image: 'partW04.jpg'
+  },
+  {
+    id: 5,
+    typeId: 1,
+    name: 'Dębica Navigator',
+    description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam blandit cursus augue sed eleifend. Donec mattis massa sit amet porttitor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer vehicula tristique laoreet. Quisque nisi eros, scelerisque sed nibh eu, pellentesque aliquet quam. Nullam euismod mattis mi, nec ultricies turpis imperdiet ac. Sed molestie massa erat, ut ullamcorper arcu scelerisque eu.'],
+    price: 120,
+    image: 'partW05.jpg'
+  },
+  {
+    id: 6,
+    typeId: 3,
+    name: 'Radio Pioneer MVH-180UB',
+    description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam blandit cursus augue sed eleifend. Donec mattis massa sit amet porttitor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer vehicula tristique laoreet. Quisque nisi eros, scelerisque sed nibh eu, pellentesque aliquet quam. Nullam euismod mattis mi, nec ultricies turpis imperdiet ac. Sed molestie massa erat, ut ullamcorper arcu scelerisque eu.'],
+    price: 120,
+    image: 'partW06.jpg'
+  },
+  {
+    id: 7,
+    typeId: 3,
+    name: 'Sony MEX-N5100BT',
+    description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam blandit cursus augue sed eleifend. Donec mattis massa sit amet porttitor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer vehicula tristique laoreet. Quisque nisi eros, scelerisque sed nibh eu, pellentesque aliquet quam. Nullam euismod mattis mi, nec ultricies turpis imperdiet ac. Sed molestie massa erat, ut ullamcorper arcu scelerisque eu.'],
+    price: 120,
+    image: 'partW07.jpg'
+  }
+]
