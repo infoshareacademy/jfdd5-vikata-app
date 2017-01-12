90b0885ac2904c997c21ad4a9e80f10197a424b8
Rafal Miler <rafalmiler77@gmail.com>
2017-01-12 16:31:00 +0100

    login reducer added

--- src/LoginPanelView/LoginPanelView.js
+++ src/LoginPanelView/LoginPanelView.js
@@ -3,13 +3,19 @@
  */
 import React from 'react'
 import { Button } from 'react-bootstrap'
+import { connect } from 'react-redux'
+
+const mapStateToProps = state =>({
+  isLogged : state.isLoggedData.isLogged
+})
 
 const LoginPanelView =(props) => (
 
         <div>
           <h1>tresc</h1>
           <Button>Login</Button>
+          {console.log(props)}
         </div>
 )
 
-export default LoginPanelView
+export default connect(mapStateToProps)(LoginPanelView)
--- src/LoginPanelView/reducer.js
+++ src/LoginPanelView/reducer.js
@@ -1,3 +1,13 @@
 /**
  * Created by rafalmiler on 12.01.17.
  */
+const initialState = {
+  isLogged:false
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
