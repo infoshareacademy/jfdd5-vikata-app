6fb0e862a61ff0d882dd5ec492a00364e43f21ee
Alwox <alanwolejko@gmail.com>
2017-01-12 16:15:39 +0100

    poprawione wyswietlanie - cena tylko przy sprzedaży

--- src/PartsListView/PartsListView.js
+++ src/PartsListView/PartsListView.js
@@ -33,48 +33,51 @@ const PartsListView = (props) => (
           ).filter(
             (part) => props.selectedModel === null ? true : props.selectedModel.partsIds.indexOf( part.id ) !== -1
           ).map(
-            (part) => (
-              <ListGroupItem key={part.id}  className="PartsListView-tile">
-                <Grid>
-                  <Row className="show-grid">
-                    <Col md={4}>
-                      <Image src={process.env.PUBLIC_URL + '/img/img-parts/' + part.image} rounded responsive/>
-                    </Col>
-                    <Col md={5}>
-                      <h2>
-                        {part.name}
-                      </h2>
-                      <p>
-                        {part.description}
-                      </p>
-                    </Col>
-                    <Col md={2}>
-                      <h2><Label bsStyle="info">Cena: {part.price}</Label></h2>
-                      {
-                        props.users.map(
-                          user => user.partsToSell.indexOf(part.id) !== -1
-                        ).some(
-                          hasPart => hasPart === true
-                        ) === true ? <h2><Label bsStyle="success">Na sprzedaż</Label></h2> : <h2><Label bsStyle="warning">Poszukiwane</Label></h2>
-                      }
+            (part) => {
+              const isWanted = props.users.map(
+                user => user.partsToSell.indexOf(part.id) !== -1
+              ).some(
+                hasPart => hasPart === true
+              )
 
-                    </Col>
-                  </Row>
-                </Grid>
+              return (
+                <ListGroupItem key={part.id}  className="PartsListView-tile">
+                  <Grid>
+                    <Row className="show-grid">
+                      <Col md={4}>
+                        <Image src={process.env.PUBLIC_URL + '/img/img-parts/' + part.image} rounded responsive/>
+                      </Col>
+                      <Col md={5}>
+                        <h2>
+                          {part.name}
+                        </h2>
+                        <p>
+                          {part.description}
+                        </p>
+                      </Col>
+                      <Col md={2}>
+                        {
+                          isWanted === true ? <div><h2><Label bsStyle="success">Na sprzedaż</Label></h2><h2><Label bsStyle="info">Cena: {part.price}</Label></h2></div> : <h2><Label bsStyle="warning">Poszukiwane</Label></h2>
+                        }
 
-                {/*                <ButtonGroup>
-                  <LinkContainer to={'/parts-list/' + part.id+""}>
-                    <Button bsStyle="info">Opis produktu</Button>
-                  </LinkContainer>
+                      </Col>
+                    </Row>
+                  </Grid>
 
-                  <LinkContainer to={'/parts-list/' + part.id + "/shops"}>
-                    <Button bsStyle="info">Lista hurtowni</Button>
-                  </LinkContainer>
-                </ButtonGroup>
-                 {part.id === parseInt(props.params.partId) ? props.children : null}*/}
+                  {/*                <ButtonGroup>
+                   <LinkContainer to={'/parts-list/' + part.id+""}>
+                   <Button bsStyle="info">Opis produktu</Button>
+                   </LinkContainer>
 
-              </ListGroupItem>
-            )
+                   <LinkContainer to={'/parts-list/' + part.id + "/shops"}>
+                   <Button bsStyle="info">Lista hurtowni</Button>
+                   </LinkContainer>
+                   </ButtonGroup>
+                   {part.id === parseInt(props.params.partId) ? props.children : null}*/}
+
+                </ListGroupItem>
+              )
+            }
           )
         }
       </ListGroup>
