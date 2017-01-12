aa99a6de1d7f6133f4c9b9bb9b5eca67d6140c92
Rafał Miler <rafalmiler77@gmail.com>
2017-01-12 23:15:28 +0100

    Grid system added in App.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,32 @@
 import React from 'react'
-import { PageHeader } from 'react-bootstrap'
+import {PageHeader, Row, Col, Grid, Clearfix} from 'react-bootstrap'
 import './App.css';
-import { CarSelectionForm } from '../CarSelectionForm'
-import { TypesView } from '../TypesView'
-import { LoginPanelView } from '../LoginPanelView'
+import {CarSelectionForm} from '../CarSelectionForm'
+import {TypesView} from '../TypesView'
+import {LoginPanelView} from '../LoginPanelView'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <PageHeader bsClass="pageHeader">Autonaprawiatch v.2.0 <small>Nietypowe pojazdy</small></PageHeader>
-        <LoginPanelView/>
-        <CarSelectionForm/>
-        <TypesView/>
-          {this.props.children}
+        <PageHeader bsClass="pageHeader">Autonaprawiatch v.2.0
+          <small>Nietypowe pojazdy</small>
+        </PageHeader>
+        <Grid>
+          <Row className="show-grid">
+            <Col md={9}>
+              <CarSelectionForm/>
+            </Col>
+            <Col md={3}>
+              <LoginPanelView/>
+            </Col>
+            <Clearfix/>
+            <Col>
+            <TypesView/>
+            {this.props.children}
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
