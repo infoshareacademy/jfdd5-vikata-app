a711dbeffc057a8f2f93d0af71e579a854c79eba
Alwox <alanwolejko@gmail.com>
2017-01-22 16:39:33 +0100

    zdjęcia teraz pojawiają się także w liście przedmiotów na sprzedaż

--- src/userWantedListView/partWantedView/PartWantedView.js
+++ src/userWantedListView/partWantedView/PartWantedView.js
@@ -2,13 +2,18 @@
  * Created by alanw on 16.01.2017.
  */
 import React from 'react'
-import {Button} from 'react-bootstrap'
+import {Button,  Image} from 'react-bootstrap'
 
 class PartWantedView extends React.Component{
 
   render(){
     return(
       <div style={{maxHeight:700 ,overflowY:'auto'}}>
+
+        <h3>Zdjęcie:</h3>
+        <Image src={process.env.PUBLIC_URL + '/img/img-parts/'+this.props.part.image} rounded responsive/>
+        <Button bsStyle="info" block>Zmień</Button>
+        <hr/>
 
         <h3>Opis:</h3>
         {this.props.part.description}
